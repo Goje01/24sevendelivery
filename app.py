e6ded58c2cff3725ebe1405a1b8a572189f92e33
goje <emmanuelgoje389@gmail.com>
2026-07-22 18:43:32 +0100

    Add contact page and app updates

--- app.py
+++ app.py
@@ -27,12 +27,17 @@ from flask import Flask
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
+
 app = Flask(
     __name__,
     template_folder=os.path.join(base_dir, 'templates'),
     static_folder=os.path.join(base_dir, 'static')
     
 )
+@app.route("/about")
+def about():
+    return render_template("contact.html", admin_email=ADMIN_EMAIL)
+
 app.secret_key = os.environ.get("FLASK_SECRET_KEY", "dev-fallback-secret-key-12345")
 
 app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024  # 4MB
